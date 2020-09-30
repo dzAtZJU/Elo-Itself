fdad2d7f4f51ad4fcceece5cce191903356f77bf
Paper <fredchowdz@gmail.com>
2020-09-30 12:19:48 +0800

    .

--- Sources/Elo_Itself/Elo_Itself.swift
+++ Sources/Elo_Itself/Elo_Itself.swift
@@ -1,3 +1,20 @@
-struct Elo_Itself {
-    var text = "Hello, World!"
+protocol Has_Critaria {
+    var critaria: [String] {
+        get
+    }
 }
+
+protocol Has_Tasks {
+    var tasks: [Task] {
+        get
+    }
+}
+
+protocol Has_AfterTasks {
+    var afterTasks: [Task] {
+        get
+    }
+}
+
+protocol Task {}
+
--- Tests/Elo_ItselfTests/Elo_ItselfTests.swift
+++ Tests/Elo_ItselfTests/Elo_ItselfTests.swift
@@ -6,7 +6,6 @@ final class Elo_ItselfTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Elo_Itself().text, "Hello, World!")
     }
 
     static var allTests = [
