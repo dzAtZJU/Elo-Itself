69acf8f51bd7709f5b2dc048d0e95cd07c450854
Paper <fredchowdz@gmail.com>
2020-10-12 12:35:09 +0800

    .

--- Sources/Elo_Itself/Execution/Critaria.swift
+++ Sources/Elo_Itself/Execution/Critaria.swift
@@ -5,6 +5,8 @@
 //  Created by Zhou Wei Ran on 2020/10/9.
 //
 
+public protocol Criteria {}
+
 public protocol Has_Criterias {
     var criterias: [String] {
         get
---
+++ Sources/Elo_Itself/Execution/Infra.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Zhou Wei Ran on 2020/10/12.
+//
+
+public protocol Infra {}
