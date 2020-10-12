6dbdb45df3d4ed396aff3c017aababd5251af997
Paper <fredchowdz@gmail.com>
2020-10-12 14:19:25 +0800

    .

---
+++ Sources/Elo_Itself/Situation.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  
+//
+//  Created by Zhou Wei Ran on 2020/10/12.
+//
+
+public struct Unknowable_and_Uncertain_Situations: Under_Expectation {}
+
+public protocol Under_Expectation {}
