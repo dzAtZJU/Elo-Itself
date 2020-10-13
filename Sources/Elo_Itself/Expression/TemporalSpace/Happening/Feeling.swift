07d571159fe331b88c280e8f1a1b8b68d7a6bac7
Paper <fredchowdz@gmail.com>
2020-10-13 13:28:32 +0800

    .

---
+++ Sources/Elo_Itself/Expression/TemporalSpace/Happening/Feeling.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  
+//
+//  Created by Zhou Wei Ran on 2020/10/13.
+//
+public protocol Feeling {}
+
+public struct Curious: Feeling {
+    public init() {}
+}
--- Sources/Elo_Itself/StartingPoint.swift
+++ Sources/Elo_Itself/StartingPoint.swift
@@ -1,10 +1,3 @@
-//
-//  Assumption&Motto.swift
-//  Semantics
-//
-//  Created by Zhou Wei Ran on 2020/9/2.
-//  Copyright © 2020 Paper Scratch. All rights reserved.
-//
 enum StartingPoint: String {
     case Think_by_Language = ""
     case A_Language = "Stimulated by individuals going through situation. Shaped by reality. Forces reality and implementation."
