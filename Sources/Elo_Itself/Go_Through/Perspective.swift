7e3dfa45d5b2070cd55069d61d5fa2d2d76c5dcd
Paper <fredchowdz@gmail.com>
2020-10-23 12:46:41 +0800

    .

--- Sources/Elo_Itself/Go_Through/Perspective.swift
+++ Sources/Elo_Itself/Go_Through/Perspective.swift
@@ -1,12 +1,13 @@
-//
-//  File.swift
-//  
-//
-//  Created by Zhou Wei Ran on 2020/10/3.
-//
-
 public protocol Perspective {}
 
-public struct Eye: Perspective {}
+public struct Eye: Perspective, Has_Drawbacks {
+    var drawbacks: [String] = ["Directional", "On Surface"]
+}
 
-public struct Ear: Perspective {}
+public struct Ear: Perspective, Has_Drawbacks {
+    var drawbacks: [String] = ["On Surface"]
+}
+
+public struct Empathy: Perspective, Has_Criterias {
+    var criterias: [Any] = ["Capture Emotion Flow"]
+}
