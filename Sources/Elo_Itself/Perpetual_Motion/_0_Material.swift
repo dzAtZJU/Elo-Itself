25aa9cce7f513452c2246d2fb31868ea592ea7fa
Paper <fredchowdz@gmail.com>
2020-12-27 03:01:43 +0800

    .

--- Sources/Elo_Itself/Perpetual_Motion/_0_Material.swift
+++ Sources/Elo_Itself/Perpetual_Motion/_0_Material.swift
@@ -10,7 +10,19 @@ public protocol Steady_Source: Material {}
 
 public protocol Under_Expectation: Material {}
 
+public protocol Opportunity: Material {}
+
 public protocol Self_Belief {}
+
+public struct SocialNetwork: Steady_Source {}
+
+public struct Love: Opportunity, Has_Materials {
+    public var materials: [Material] {
+        [Task()]
+    }
+    
+    struct Task: Under_Expectation {}
+}
 
 public struct Unknowable_and_Uncertain_Situations: Under_Expectation {
     public init() {}
